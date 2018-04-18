750db4e3b12967b329eadac6e3f3779f9caf8ac2
balintpinke <pinke.balint@gmail.com>
2018-04-18 21:42:34 +0200

    leave out plotly, fixing axes in plot1 fixing dataset for datatable

--- app.R
+++ app.R
@@ -2,19 +2,28 @@ library(ggplot2)
 library(dplyr)
 library(shiny)
 library(shinythemes)
-library(plotly)
+library(radarchart)
 
 NHL <- read.csv("NHL_cleaned.csv", sep =";", header = TRUE)
 
 server <- function(input, output) {
-  output$plot <- renderPlotly({
+  output$plot <- renderPlot({
     p <- ggplot(NHL, 
            aes(x = select_if(NHL, is.numeric)[,input$xcol],
                y = select_if(NHL, is.numeric)[,input$ycol],
-               color = select_if(NHL, is.numeric)[,input$filter] > input$number)) 
+               color = select(NHL, Age, GP:PlusMinus, TOI.GP, PIM)[,input$filter] > input$number)) 
     p <- p + geom_point()
-    p <- p + geom_smooth(method = "lm")
-    ggplotly(p)
+    p <- p + geom_smooth(method = "lm") 
+    p <- p + labs(title = "2D plot with filter variable")
+    p <- p + xlab(paste(input$xcol, collapse = " "))
+    p <- p + ylab(paste(input$ycol,collapse = " "))
+    #p <- p + labs(paste(input$filter > input$number, collapse = " " ))
+    print(p)
+  })
+  
+  output$radar <- renderChartJSRadar({
+    chartJSRadar(NHL[, c("Label", input$player1)], 
+                 maxScale = 10, showToolTipLabel=TRUE)
   })
   
   output$summary <- renderPrint({
@@ -22,9 +31,9 @@ server <- function(input, output) {
   })
   
   output$table <- DT::renderDataTable(DT::datatable({
-    data <- NHL
+    data <- NHL[,1:12]
     if (input$name != "All") {
-      data <- data[data$Last.Name == input$name,]
+      data <- data[data$Name == input$name,]
     }
     if (input$nat != "All") {
       data <- data[data$Nat == input$nat,]
@@ -55,23 +64,26 @@ ui <- fluidPage(theme = shinytheme("united"),
                                                     choices=colnames(select_if(NHL, is.numeric))),
                                         selectInput("ycol", "Y Variable", 
                                                     choices=colnames(select_if(NHL, is.numeric))),
-                                        selectInput("filter", "Filter Numeric Variable", 
-                                                    choices=colnames(select_if(NHL, is.numeric))),
-                                        numericInput('number', 'Numeric value for filter Numeric Variable', 3,
-                                                     min = 1, max = 100)
+                                        selectInput("filter", "Filter Variable", 
+                                                    choices=colnames(select(NHL, Age, GP:PlusMinus, TOI.GP, PIM))),
+                                        sliderInput('number', 'set value for filter variable', 25,
+                                                     min = 0, max = 100)
                                       ),
                                       mainPanel(
-                                        plotlyOutput("plot")
+                                        plotOutput("plot")
                                       )
                                     )
                            ),
                            tabPanel("Radar plot",
                                     selectInput("player1", "Player 1", 
-                                                choices=NHL$fullname),
+                                                choices=NHL$Name),
                                     selectInput("player2", "Player 2", 
-                                                choices=NHL$fullname),
+                                                choices=NHL$Name),
                                     selectInput("plaer3", "Player 3", 
-                                                choices=NHL$fullname)
+                                                choices=NHL$Name),
+                                    mainPanel(
+                                      chartJSRadarOutput("radar", width = "450", height = "300"), width = 7
+                                    )
                              
                              ),
                            tabPanel("Descriptive Stats",
@@ -83,9 +95,9 @@ ui <- fluidPage(theme = shinytheme("united"),
                                              fluidRow(
                                                column(4,
                                                       selectInput("name",
-                                                                  "Last Name:",
+                                                                  "Name:",
                                                                   c("All",
-                                                                    unique(as.character(NHL$Last.Name))))
+                                                                    unique(as.character(NHL$Name))))
                                                ),
                                                column(4,
                                                       selectInput("nat",
